746eb78d11fffc8f12abb1ae0aa1bdbf77fc5760
BenteO <ottben14@student.westerdals.no>
2015-04-28 22:20:05 +0200

    Begynt å animere etc Mario

---
+++ Assets/Scripts/Mario.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mario : MonoBehaviour {
+	public float maxSpeed;
+	//public float acceleration = 0.5f;
+	//public float jumpForce = 700f;
+
+	private bool facingRight = true;
+
+	Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		anim.SetFloat("Speed", h);
+		transform.Translate (h * maxSpeed * Time.deltaTime, 0, 0);
+
+
+		if (h > 0 && !facingRight) Flip();
+		else if (h < 0 && facingRight) Flip();
+	}
+
+
+	void Flip () {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+}
