6f9b9bfa8e4fe37bad305e0a9aa00a94b129bcb7
BenteO <ottben14@student.westerdals.no>
2015-04-29 20:05:52 +0200

    Lagt til hopping og hoppeanimasjon, endret layer på bricks mm til ground (Hoppingen er fucked, så må fikses...)

--- Assets/Scripts/Mario.cs
+++ Assets/Scripts/Mario.cs
@@ -4,17 +4,33 @@ using System.Collections;
 public class Mario : MonoBehaviour {
 	public float maxSpeed = 8;
 	//public float acceleration = 0.5f;
-	public float jumpForce = 700f;
+	public float jumpSpeed = 0.5f;
 
 	private bool facingRight = true;
 
-	Animator anim;
+	private Animator anim;
+	private Transform GroundCheck;
+	public LayerMask GroundLayers;
 
 	void Start () {
 		anim = GetComponent<Animator>();
+		GroundCheck = transform.FindChild("GroundCheck");
 	}
 
 	void Update () {
+		bool isGrounded = Physics2D.OverlapPoint(GroundCheck.position, GroundLayers);
+
+		if (Input.GetButton("Jump")) {
+
+			if(isGrounded) {
+				Debug.Log("Jump!");
+				GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+				isGrounded = false;
+			}
+		}
+
+		anim.SetBool("isGrounded", isGrounded);
+
 		float h = Input.GetAxis("Horizontal");
 		anim.SetFloat("Speed", h);
 		transform.Translate (h * maxSpeed * Time.deltaTime, 0, 0);
@@ -29,8 +45,6 @@ public class Mario : MonoBehaviour {
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
-		float h = Input.GetAxis("Horizontal");
-		anim.SetFloat("lSpeed", h);
 	}
 
 }
